700e8dcfed4df5a52c879d2892d3773bb4c07193
sali0501 <salik_89@hotmail.dk>
2014-02-25 10:59:20 +0100

    Early state of ProducerComsumer

---
+++ BoundedBuffer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    class BoundedBuffer
+    {
+        private readonly int _capasity;
+        private Queue<int> queue = new Queue<int>(); 
+
+        public BoundedBuffer(int capasity)
+        {
+            if (capasity < 1)
+            {
+                throw new ArgumentException();
+            }
+
+            _capasity = capasity;
+        }
+
+        public bool IsFull()
+        {
+            return queue.Count == _capasity;
+        }
+
+        public void Put(int element)
+        {
+            Monitor.Enter(queue);
+            try
+            {
+                if (IsFull())
+                {
+                    Monitor.Wait(queue);
+                }
+                queue.Enqueue(element);
+            }
+            finally
+            {
+                Monitor.Exit(queue);
+                Monitor.Pulse(queue);
+            }
+            
+        }
+
+        public int Take()
+        {
+            Monitor.Enter(queue);
+            int temp;
+            try
+            {
+                if (queue.Count == 0)
+                {
+                    Monitor.Wait(queue);
+                }
+                temp = queue.Dequeue();
+            }
+            finally
+            {
+                Monitor.Exit(queue);
+                Monitor.Pulse(queue);
+            }
+            return temp;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    class Program
+    {
+        static BoundedBuffer bb = new BoundedBuffer(10);
+
+        static void Main(string[] args)
+        {
+            Thread produce = new Thread(Producer);
+            //Thread comsumer = new Thread(Consumer);
+
+            
+        }
+
+        static void Producer()
+        {
+            Console.WriteLine("Producer starts!");
+            Console.ReadKey();
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Random rnd = new Random();
+                int number = rnd.Next(1, 100);
+                bb.Put(number);
+                Console.WriteLine("Put: " + number);
+            }
+        }
+
+        static void Consumer()
+        {
+            while (true)
+            {
+                Thread.Sleep(700);
+                int taken = bb.Take();
+                Console.WriteLine("Take: " + taken);
+            }
+        }
+    }
+}
